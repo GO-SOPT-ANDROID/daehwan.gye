fee2e9c40df4ef6eeb574cd00d21e689ff9207ec
머환임 <udsward@gmail.com>
2023-04-07 23:12:04 +0900

    Profile 구현

--- app/src/main/java/org/android/go/sopt/external/view/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/external/view/SignUpActivity.kt
@@ -1,11 +1,11 @@
 package org.android.go.sopt.external.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.android.go.sopt.R
+import org.android.go.sopt.*
 import org.android.go.sopt.databinding.ActivitySignUpBinding
-import org.android.go.sopt.idSucceed
-import org.android.go.sopt.pwSucceed
+import org.android.go.sopt.internal.signup.SignUpUser
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignUpBinding
@@ -15,21 +15,37 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_sign_up)
 
-        onClick();
+        onClick()
     }
 
     private fun onClick() {
         btnSignIn()
-
     }
 
     private fun btnSignIn() {
         binding.btnSignUp.setOnClickListener {
             with(binding) {
                 if (idSucceed() && pwSucceed()) {
-                    // do something
+                    val toProfile = Intent(this@SignUpActivity, ProfileActivity::class.java)
+                    toProfile.putExtra(
+                        "SignUpUser",
+                        SignUpUser(
+                            id = binding.etId.get(),
+                            password = binding.etPw.get(),
+                            name = binding.etName.get(),
+                            hobby = binding.etHobby.get()
+                        )
+                    )
+                    toast("안녕하세요!")
+                    finish()
+                } else {
+                    toast("id 혹은 password 가 정상적으로 입력되지 않았습니다.")
                 }
             }
         }
+    }
+
+    companion object {
+        const val SIGN_UP_USER = "signUpUser"
     }
 }
