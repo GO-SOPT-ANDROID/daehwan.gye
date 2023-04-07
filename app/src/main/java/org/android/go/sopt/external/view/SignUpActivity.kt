3fe41caf954edac0c9cb2e0c73453bd2f2aca935
머환임 <udsward@gmail.com>
2023-04-07 23:37:05 +0900

    extra

--- app/src/main/java/org/android/go/sopt/external/view/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/external/view/SignUpActivity.kt
@@ -3,17 +3,20 @@ package org.android.go.sopt.external.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.android.go.sopt.*
 import org.android.go.sopt.databinding.ActivitySignUpBinding
+import org.android.go.sopt.get
+import org.android.go.sopt.idSucceed
 import org.android.go.sopt.internal.signup.SignUpUser
+import org.android.go.sopt.pwSucceed
+import org.android.go.sopt.toast
 
 class SignUpActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySignUpBinding
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
 
         onClick()
     }
@@ -24,24 +27,24 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun btnSignIn() {
         binding.btnSignUp.setOnClickListener {
-            with(binding) {
-                if (idSucceed() && pwSucceed()) {
-                    val toProfile = Intent(this@SignUpActivity, ProfileActivity::class.java)
-                    toProfile.putExtra(
-                        "SignUpUser",
-                        SignUpUser(
-                            id = binding.etId.get(),
-                            password = binding.etPw.get(),
-                            name = binding.etName.get(),
-                            hobby = binding.etHobby.get()
-                        )
+            if (binding.idSucceed() && binding.pwSucceed()) {
+                val toProfile = Intent(this@SignUpActivity, ProfileActivity::class.java)
+                toProfile.putExtra(
+                    SIGN_UP_USER,
+                    SignUpUser(
+                        id = binding.etId.get(),
+                        password = binding.etPw.get(),
+                        name = binding.etName.get(),
+                        hobby = binding.etHobby.get()
                     )
-                    toast("안녕하세요!")
-                    finish()
-                } else {
-                    toast("id 혹은 password 가 정상적으로 입력되지 않았습니다.")
-                }
+                )
+                toast("안녕하세요!")
+                startActivity(toProfile)
+                finish()
+            } else {
+                toast("id 혹은 password 가 정상적으로 입력되지 않았습니다.")
             }
+
         }
     }
 
